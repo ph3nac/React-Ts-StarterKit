3c0562275822e248a4e3c271833c5b2bc271dc4a
ph3 <100564304+ph3nac@users.noreply.github.com>
2022-11-22 15:44:51 +0900

    remove content and update BookmarkBar

--- src/components/BookmarkBar/BookmarkBarMock.ts
+++ src/components/BookmarkBar/BookmarkBarMock.ts
@@ -1,13 +1,13 @@
-import { BarProps, BookmarkProps, FolderProps } from '@/@types/BookmarkTyep';
+import { BarProps, ContentProps } from '@/@types/BookmarkTyep';
 import { FolderMock } from '../Folder/FolderMock';
 import { BookmarkMock } from '../Bookmark/BookmarkMock';
 
-const folders: FolderProps[] = Array.from({ length: 5 }, (v, i) => ({
+const contents: ContentProps[] = Array.from({ length: 5 }, (v, i) => ({
   ...FolderMock,
   id: i.toString(),
 }));
 
-const bookmarks: BookmarkProps[] = Array.from({ length: 5 }, (v, i) => ({
+const bookmarks: ContentProps[] = Array.from({ length: 5 }, (v, i) => ({
   ...BookmarkMock,
   id: i.toString(),
 }));
@@ -17,6 +17,5 @@ export const BookmarkBarMock: BarProps = {
   add_date: '000000',
   last_modified: '000000',
   personal_toolbar_folder: true,
-  folders: folders,
-  bookmarks: bookmarks,
+  contents: contents,
 };
